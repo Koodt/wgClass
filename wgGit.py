32013d2e2174898b3a1b1eff474989451444f51c
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-04 17:57:21 +0300

    Update wgGit.py

--- wgGit.py
+++ wgGit.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 
 class GitClass(object):
-    def __init__(self):
+    def __init__(self, repoTarget):
         from git import Repo
         self.Repo = Repo
+        self.repoTarget = repoTarget
 
     def dlRepo(self, url, path):
         self.url = url
         self.path = path
         self._repo = self.Repo.clone_from(self.url, self.path)
 
-    def createNewBranch(self, repoTarget, branch):
-        self.repoTarget = repoTarget
+    def createNewBranch(self, branch):
         self.branch = branch
         self.repoW = self.Repo(self.repoTarget)
         self.newBranch = self.repoW.create_head(self.branch)
 
-    def selectNeededBranch(self, repoTarget, branch):
-        self.repoTarget = repoTarget
+    def selectNeededBranch(self, branch):
         self.branch = branch
         self.repoW = self.Repo(self.repoTarget)
         self.branchSwitch = self.repoW.git.checkout(self.branch)
 
-    def selectMasterBranch(self, repoTarget):
-        self.repoTarget = repoTarget
+    def selectMasterBranch(self):
         self.repoW = self.Repo(self.repoTarget)
         self.new_branch = self.repoW.heads.master.checkout()
+
+    def commitAndPush(self, repoTarget):
+        self.repoTarget = repoTarget
+        self
 
     @property
     def repo(self):
@@ -35,8 +37,8 @@ class GitClass(object):
 
 
 if __name__ == '__main__':
-    wgClass = GitClass()
-    wgClass.dlRepo('https://github.com/Koodt/wgClass.git', '/srv/kill')
-    wgClass.createNewBranch('/srv/kill', 'morenewBranch')
-    wgClass.selectNeededBranch('/srv/kill', 'morenewBranch')
+    wgClass = GitClass('/srv/kill')
+    wgClass.dlRepo('https://github.com/Koodt/wgClass.git')
+    wgClass.createNewBranch('morenewBranch')
+    wgClass.selectNeededBranch('morenewBranch')
 #    wgClass.selectMasterBranch('/srv/kill')
